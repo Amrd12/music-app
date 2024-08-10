d01b10a962fcc5846175d3ea9749da6b0a0a2909
Amrd12 <elnbawya@gmail.com>
2024-08-10 16:45:24 +0300

    model

--- lib/models/model_info/info_music_model.dart
+++ lib/models/model_info/info_music_model.dart
@@ -43,22 +43,3 @@ class InfoMusicModel extends MainModel with HiveObjectMixin {
   @override
   ModelType get type => ModelType.video;
 }
-
-// Challenge
-// Create a class Customer with three properties: name, age, and phone.
-// The class should have one constant constructor. The constructor should
-
-//  initialize the values of the three properties. Create an object of the
-//   class Customer and print the values of the three properties.
-class Customer {
-  final String name;
-  final int age;
-  final int phone;
-
-  const Customer(this.name, this.age, this.phone);
-}
-
-void main(List<String> args) {
-  const Customer cus = Customer("name", 13, 313);
-  print("name ${cus.name} age: ${cus.age} , phone: ${cus.phone}");
-}
